33588694d2963f0934c0c0d3b5e4510f37928e22
masterUNG <phrombutr@gmail.com>
2019-04-18 03:01:01 +0700

    Project Complete

--- UngMarket2/RegisterViewController.swift
+++ UngMarket2/RegisterViewController.swift
@@ -58,17 +58,71 @@ class RegisterViewController: UIViewController {
 //        Check Space
         if ((name.count == 0) || (user.count == 0) || (password.count == 0)) {
             print("Have Space")
+            myAlert(titleString: "Have Space", messageString: "Please Fill Every Blank")
         } else {
             print("No Space")
+            uploadDataToServer(name: name, user: user, password: password)
         }
+    
+    }   // uploadButton
+    
+//    Process Upload Data to Server
+    func uploadDataToServer(name: String, user: String, password: String) -> Void {
+        
+        let urlString = "https://www.androidthai.in.th/ssm/addDataMaster.php?isAdd=true&Name=\(name)&User=\(user)&Password=\(password)"
+        
+        let url = URL(string: urlString)
+        let request = NSMutableURLRequest(url: url!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+            if error != nil {
+                print("Have Error")
+            } else {
+                
+                if let testReadAble = data {
+                    
+                    let canReadData = NSString(data: testReadAble, encoding: String.Encoding.utf8.rawValue)
+                    print("canReadData ==> \(String(describing: canReadData))")
+                    
+                    let myResult = canReadData!
+                    if myResult == "true" {
+                        
+                        DispatchQueue.main.async {
+                             self.performSegue(withIdentifier: "BackToAuthen", sender: nil)
+                        }
+                        
+                    }
+                    
+                    
+                    
+                } else {
+                    print("Canot Read Able")
+                }   // if2
+                
+            } // if1
+        } // End task
+        task.resume()
         
         
         
         
         
         
+    }   // uploadData
+    
+//    myAlert is void Type Function ก็คือ เมธอด ที่ทำงานแล้วไม่คือค่า ให้ คนสั่ง
+    func myAlert(titleString: String, messageString: String) -> Void {
         
-    }
+//        Create Alert Object
+        let objAlert = UIAlertController(title: titleString, message: messageString, preferredStyle: UIAlertController.Style.alert)
+        
+//        Create Button on Alert
+        objAlert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            objAlert.dismiss(animated: true, completion: nil)
+        }))
+        
+        present(objAlert, animated: true, completion: nil)
+        
+    }   //
     
     
 
--- UngMarket2/ViewController.swift
+++ UngMarket2/ViewController.swift
@@ -8,12 +8,123 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//Hot to Hide Keyboard by Impliment UITextFieldDelegate
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    let titleHaveSpace: String = "มีช่องว่าง"
+    let messageHaveSpace: String = "กรุณากรอกทุกช่อง คะ"
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.userTextField.delegate = self
+        self.passwordTextField.delegate = self
+        
+        
         // Do any additional setup after loading the view.
     }   // Main Method
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
+        let user = userTextField.text!
+        let password = passwordTextField.text!
+        
+        if ((user.count == 0) || (password.count == 0)) {
+            myAlert(title: titleHaveSpace, message: messageHaveSpace)
+        }   else {
+            checkUserAndPass(user: user, password: password)
+        }
+        
+    }   // loginButton
+    
+    func checkUserAndPass(user: String, password: String) -> Void {
+        
+        let urlString: String = "https://www.androidthai.in.th/ssm/getUserWhereUserMaster.php?isAdd=true&User=\(user)"
+        
+        guard let url = URL(string: urlString) else {
+            print("Have Error")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let dataResponse = data, error == nil else {
+                return
+            }
+            
+            do {
+                
+                let jsonResponse = try JSONSerialization.jsonObject(with: dataResponse, options: [])
+                print("jsonResponse ==> \(jsonResponse)")
+                
+                guard let jsonArray = jsonResponse as? [[String: Any]] else {return}
+                print("jsonArray ==> \(jsonArray)")
+                
+                let jsonDictionary: Dictionary = jsonArray[0]
+                
+                let truePassword: String = jsonDictionary["Password"] as! String
+                print("truePassword ==> \(truePassword)")
+                
+                if password == truePassword {
+                    print("Welcome to App")
+                    DispatchQueue.main.async {
+                        self.myAlert(title: "Authen True", message: "Welcome to my App")
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        self.myAlert(title: "Password False", message: "Please Try Again")
+                    }
+                }
+                
+                
+            } catch let myError {
+                DispatchQueue.main.async {
+                    print(myError)
+                    self.myAlert(title: "User False", message: "No \(user) in my Database")
+                }
+            }
+            
+        }   // end task
+        task.resume()
+        
+        
+        
+        
+    }   // checkUserAndPass
+    
+    
+    
+    
+    func myAlert(title: String, message: String) -> Void {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
     
     
     
